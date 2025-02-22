dcfe9e31bd2b90893e86951b64b7e6d6045b7b0a
Rahul Behera <rahulbehera@Rahuls-MacBook-Air.local>
2025-02-22 13:45:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+# BioticPlaza
+=======
+
+#Plant-Selling-Ecommerce-Website
+
+Create a complete responsive online ecommerce website design for beginers using html css and javascript step by step.
+
+The main feature of this website are:
+
+✔ Responsive sticky triple header / navbar with toggle menu bar effect.
+   
+✔ Responsive shopping cart box.
+
+✔ Responsive home with touch slider effect using swipper.js
+
+✔ Responsive banner card section using flexbox.
+
+✔ Responsive category card section using flexbox.
+
+✔ Responsive product card section using flexbox.
+
+✔ Responsive deal section with count down effect using flexbox.
+
+✔ Responsive and animated contact form section using flexbox.
+
+✔ Responsive footer section using flexbox.
+
+Key: #Ecommerce-Website, #Plant-shop, #FrontEnd
+>>>>>>> 0adf958 (Initial commit)
---
+++ js/script.js
@@ -0,0 +1,119 @@
+const barmenu = document.querySelector('.nav-bar');
+let navbars = document.querySelector('#menu-bar');
+let header3 = document.querySelector('.header-3');
+let scrollTop = document.querySelector('.scroll-top');
+document.querySelector("#menu-bar").onclick = () => {
+    navbars.classList.toggle('fa-times');
+    barmenu.classList.toggle('active');
+    cart.classList.remove('active');
+}
+
+
+window.onscroll = () => {
+
+    navbars.classList.remove('fa-times');
+    barmenu.classList.remove('active');
+    if (window.scrollY > 250) {
+        header3.classList.add('active');
+    } else {
+        header3.classList.remove('active');
+    }
+    if (window.scrollY > 250) {
+        scrollTop.style.display = 'initial';
+    } else {
+        scrollTop.style.display = 'none';
+    }
+
+}
+window.onload = () => {
+    cart.classList.remove('active');
+    userlogin.classList.remove('active');
+    navbars.classList.remove('fa-times');
+    barmenu.classList.remove('active');
+}
+const userlogin = document.querySelector('.login-form-container');
+document.querySelector('#login-btn ').onclick = () => {
+    userlogin.classList.toggle('active');
+    navbars.classList.remove('fa-times');
+    barmenu.classList.remove('active');
+    cart.classList.remove('active');
+}
+const closeLogin = document.querySelector('.login-form-container');
+document.querySelector('#close-login-btn').onclick = () => {
+    closeLogin.classList.remove('active');
+
+}
+let cart = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.toggle('active');
+    navbars.classList.remove('fa-times');
+    barmenu.classList.remove('active');
+
+}
+let swiper = new Swiper(".home-slider", {
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
+let countDate = new Date('june 1, 2023 00:00:00').getTime();
+
+function countDown() {
+
+    let now = new Date().getTime();
+
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function () {
+    countDown();
+}, 1000)
+
+
+const signupLink = document.getElementById("signup-link");
+const loginLink = document.getElementById("login-link");
+const loginForm = document.querySelector(".login-form-container");
+const signupForm = document.querySelector(".signup-form-container");
+const closeLoginBtn = document.getElementById("close-login-btn");
+const closeSignupBtn = document.getElementById("close-signup-btn");
+
+signupLink.addEventListener("click", function () {
+    loginForm.style.display = "none";
+    signupForm.style.display = "block";
+});
+
+loginLink.addEventListener("click", function () {
+    signupForm.style.display = "none";
+    loginForm.style.display = "block";
+});
+
+closeLoginBtn.addEventListener("click", function () {
+    loginForm.style.display = "none";
+});
+
+closeSignupBtn.addEventListener("click", function () {
+    signupForm.style.display = "none";
+});
